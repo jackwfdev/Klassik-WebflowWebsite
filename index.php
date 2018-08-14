22a857d8f0910a09dff4a931ff55f3363d415554
jackwfdev <jackcontreras262@gmail.com>
2018-08-14 17:50:03 +0530

    Fonts : Make the loading of fonts asynchronous so that it don't block rendering

--- index.php
+++ index.php
@@ -35,8 +35,6 @@
     }
 
   </script>
-  <script src="https://ajax.googleapis.com/ajax/libs/webfont/1.4.7/webfont.js" type="text/javascript"></script>
-  <script type="text/javascript">WebFont.load({  google: {    families: ["Poppins:200,200italic,300,300italic,regular,italic,500,500italic,600,600italic,700,700italic,800,800italic,900,900italic","Lora:regular,italic,700,700italic"]  }});</script>
   <!-- [if lt IE 9]><script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.min.js" type="text/javascript"></script><![endif] -->
   <script type="text/javascript">!function(o,c){var n=c.documentElement,t=" w-mod-";n.className+=t+"js",("ontouchstart"in o||o.DocumentTouch&&c instanceof DocumentTouch)&&(n.className+=t+"touch")}(window,document);</script>
   <!--
@@ -830,7 +828,7 @@
       </div>
     </div>
   </div>
-  <div data-ix="preloader" class="preloader">
+  <div data-ix="preloader" class="preloader js_preloader">
     <div class="preloader-icon"><img src="images/oval.svg"></div>
   </div>
   <script type="text/javascript" src="js/jQuery-v3.3.1.min.js"></script>
@@ -855,6 +853,22 @@
   <script src="js/modules/time-and-date.js" type="text/javascript"></script>
   <script src="js/modules/user.js" type="text/javascript"></script>
   <script src="js/forms.js" type="text/javascript"></script>
+  <!-- <script src="js/page-load.js" type="text/javascript"></script> -->
   <!-- [if lte IE 9]><script src="https://cdnjs.cloudflare.com/ajax/libs/placeholders/3.0.2/placeholders.min.js"></script><![endif] -->
+  <script type="text/javascript">
+    var WebFontConfig = {
+      google: {
+        families: [
+          "Poppins:200,200italic,300,300italic,regular,italic,500,500italic,600,600italic,700,700italic,800,800italic,900,900italic","Lora:regular,italic,700,700italic"
+        ]
+      }
+    };
+    ( function ( d ) {
+      var wf = d.createElement( "script" ), s = d.scripts[ 0 ];
+      wf.src = "https://ajax.googleapis.com/ajax/libs/webfont/1.6.26/webfont.js";
+      wf.async = true;
+      s.parentNode.insertBefore( wf, s );
+    } )( document );
+  </script>
 </body>
 </html>
